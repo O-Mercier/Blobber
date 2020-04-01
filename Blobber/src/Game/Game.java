78d60c3a617404de6b04c4363968d63e23f27fca
Antoine <antoineccmm@msn.com>
2020-04-01 11:13:03 -0400

    02

--- Blobber/src/Game/Game.java
+++ Blobber/src/Game/Game.java
@@ -4,16 +4,21 @@ import UI.*;
 
 public class Game {
 	private AppWindow appWindow;
+	private Player player;
 	
 	public Game () {
 		appWindow = new AppWindow(this);
 	}
 	
 	public void startGame(String name) {
-		
+		player = new Player(name);
 	}
 	
 	public void endGame() {
 		
+	}
+	
+	public Player getPlayer() {
+		return player;
 	}
 }
