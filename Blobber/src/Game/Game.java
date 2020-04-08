57dfd33fbc66c9ae4d25149022055a156a096dc8
Antoine <antoineccmm@msn.com>
2020-04-08 12:51:16 -0400

    13-MethodFactory-and-BlobFactory

--- Blobber/src/Game/Game.java
+++ Blobber/src/Game/Game.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import Blob.ABlob;
 import Blob.Blob;
+import Blob.BlobFactory;
 import Cursor.ACursorDecorator;
 import Cursor.BaseCursor;
 import Cursor.ICursor;
@@ -11,16 +12,12 @@ import Popper.SingleClickPopper;
 import UI.*;
 
 public class Game implements LoopObserver {
-	private int currentHP = 1;
-	private final int MAX_DAMAGE = 19, MIN_TICKS = 20, MAX_TICKS = 20, BLOB_WIDTH = 20;
-	private int max_w = 800, max_h = 600;
-	private Random rand = new Random();
 	private int ticksBeforeNewBlob, ticksBeforeNewBlobCounter;
-	
 	private ICursor c;
 	private AppWindow appWindow;
 	private Player player;
 	private GameFrame gameFrame;
+	private BlobFactory blobFactory; 
 	
 	public Game () {
 		appWindow = new AppWindow(this);
@@ -28,6 +25,7 @@ public class Game implements LoopObserver {
 	}
 	
 	public void startGame(String name) {
+		blobFactory = new BlobFactory(gameFrame.getWidth(), gameFrame.getHeight());
 		c = new BaseCursor();
 		player = new Player(name);
 		GameLoop.getInstance().registerObserver(this);
@@ -57,9 +55,7 @@ public class Game implements LoopObserver {
 			ticksBeforeNewBlob--;
 		} else {	
 			System.out.println("Creation dun blob");
-			int d = w();
-			Blob b = new Blob(ticks(),damage(),currentHP,currentHP,x(d),y(d),d,this);
-			b.addMouseListener(new SingleClickPopper(b));
+			ABlob b = blobFactory.createBlob(this);
 			gameFrame.addBlob(b);
 			
 			// logique de creation des blobs
@@ -70,27 +66,6 @@ public class Game implements LoopObserver {
 
 	public void removeBlobFromGame(ABlob b) {
 		gameFrame.removeBlob(b);
-	}
-	
-	
-	/**/
-	private int w() {
-		return 5+rand.nextInt(BLOB_WIDTH);
-	}
-
-	private int x(int w) {
-		return rand.nextInt(max_w-w);
-	}
-
-	private int y(int h) {
-		return rand.nextInt(max_h-h);
-	}
-	
-	private int ticks() {
-		return MIN_TICKS+rand.nextInt(MAX_TICKS);
-	}
-	private int damage() {
-		return 1+rand.nextInt(MAX_DAMAGE);
 	}
 	
 	public void decorateCursor(ACursorDecorator cd) {
